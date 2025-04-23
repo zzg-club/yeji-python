11ac19bcc8b19f6b33cf5b10c2ec431a7ded6ec2
solux <lee.yeji0327@gmail.com>
2025-04-23 22:18:27 +0900

    기출-04 만들 수 없는 금액 모르겠어요..

---
+++ itscote/greedy/make-money.py
@@ -0,0 +1,17 @@
+# 만들 수 없는 금액
+
+n = int(input())
+coin_list = list(map(int, input().split()))
+
+coin_list.sort()
+
+num = 1
+
+while True:
+    for coin in coin_list:
+        if num = coin: break
+        else: 
+            coin 
+
+
+# 진짜 모르겠어요..
