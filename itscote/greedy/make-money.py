ecf70a6f85aaff55cd54e510b19375cf4d34ca88
solux <lee.yeji0327@gmail.com>
2025-04-28 15:35:31 +0900

    fix: 기출-04 만들 수 없는 금액

--- itscote/greedy/make-money.py
+++ itscote/greedy/make-money.py
@@ -7,7 +7,7 @@ import itertools
 
 # 동전으로 만들 수 있는 모든 조합을 구한 리스트 1개 ~ n개
 coin_comb = []
-for i in range(1, len(coin_list)):
+for i in range(1, len(coin_list)+1):
     coin_comb.append(list(itertools.combinations(coin_list, i)))
 
 # print(coin_comb)
@@ -20,7 +20,7 @@ for case in coin_comb:
 
 # 중복된 원소 제거를 위해 set으로 만들었다가, 다시 리스트로 만듦
 coin_set = set(coin_sum)
-# print(coin_set)
+# print(coin_set)   
 coin_case = list(coin_set)
 
 # 중복된 원소가 제거된 리스트를 오름차순 정렬
@@ -28,7 +28,7 @@ coin_case.sort()
 
 # 1부터 coin_case의 길이 만큼 루프를 돌면서 1부터 모든 수가 있는지 확인
 # 없다면, 모든 경우의 수를 따져도 해당 수를 만들 수 없는 것이므로 print 후 break
-for i in range(1, len(coin_case)):
+for i in range(1, len(coin_case)+1):
     if i == coin_case[i-1]:
         continue
     else:
