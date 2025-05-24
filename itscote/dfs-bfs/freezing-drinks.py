9d08b2e502a417cfb54b8d1be13d015e24abae11
solux <lee.yeji0327@gmail.com>
2025-05-24 21:05:46 +0900

    fix: 음료 얼려 먹기

--- itscote/dfs-bfs/freezing-drinks.py
+++ itscote/dfs-bfs/freezing-drinks.py
@@ -39,7 +39,7 @@ n, m = map(int, input().split())
 # 2차원 리스트의 맵 정보 입력받기
 graph = []
 for i in range(n):
-		graph.append(list(map(int, input()))
+		graph.append(list(map(int, input())))
 
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 def dfs(x,y):
