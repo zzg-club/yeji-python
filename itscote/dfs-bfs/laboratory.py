b80747b70986bcfa744be79ef2ce9109d6c123e2
solux <lee.yeji0327@gmail.com>
2025-05-24 21:16:12 +0900

    add: 기출-16 연구소 보완

--- itscote/dfs-bfs/laboratory.py
+++ itscote/dfs-bfs/laboratory.py
@@ -1,3 +1,6 @@
+# 기출-16 연구소
+# https://www.acmicpc.net/problem/14502
+
 n, m = map(int, input().split())
 map_info = [list(map(int, input().split())) for _ in range(n)]  # 입력 오류 수정
 
@@ -45,3 +48,56 @@ for i in range(n):
             result += 1
 
 print(result)
+
+
+# --------------------------------
+
+from itertools import combinations
+import copy
+
+n, m = map(int, input().split())
+map_info = [list(map(int, input().split())) for _ in range(n)]
+
+ud = [-1, 0, 1, 0]
+lr = [0, 1, 0, -1]
+
+def virus(x, y, temp_map):
+    for i in range(4):
+        nx = x + ud[i]
+        ny = y + lr[i]
+        if 0 <= nx < n and 0 <= ny < m and temp_map[nx][ny] == 0:
+            temp_map[nx][ny] = 2
+            virus(nx, ny, temp_map)
+
+def get_safe_area(temp_map):
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if temp_map[i][j] == 0:
+                result += 1
+    return result
+
+# 0인 공간 위치 수집
+empty_spaces = [(i, j) for i in range(n) for j in range(m) if map_info[i][j] == 0]
+
+max_safe = 0
+
+# 벽 3개 세우는 조합들
+for walls in combinations(empty_spaces, 3):
+    temp_map = copy.deepcopy(map_info)
+
+    # 벽 세우기
+    for x, y in walls:
+        temp_map[x][y] = 1
+
+    # 바이러스 퍼뜨리기
+    for i in range(n):
+        for j in range(m):
+            if temp_map[i][j] == 2:
+                virus(i, j, temp_map)
+
+    # 안전 영역 계산
+    safe = get_safe_area(temp_map)
+    max_safe = max(max_safe, safe)
+
+print(max_safe)
