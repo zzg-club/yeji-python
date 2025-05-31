1e63bf946731bd88c67dd019e0a050d4a8ff8b2d
solux <lee.yeji0327@gmail.com>
2025-05-31 21:55:50 +0900

    feat: 기출-19 연산자 끼워넣기 최종코드 완성

--- itscote/dfs-bfs/insert-operator.py
+++ itscote/dfs-bfs/insert-operator.py
@@ -18,18 +18,29 @@ for i in range(len(op_info)):
 nPr = itertools.permutations(op, len(op))
 op_perm = list(nPr)
 
+# 문제 조건에 맞는 사칙연산 함수
+def calc(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        if a < 0:
+            return -(-a // b)
+        else:
+            return a // b
 
+# 모든 순열 경우의 수에 따른 연산 결과를 담을 배열
 result = []
 
+# 순열 케이스들을 순회하며 연산 후 결과 리스트에 저장
 for case in op_perm:
-    exp = []
+    res = num[0]
     for i in range(len(case)):
-        exp.append(str(num[i]))
-        exp.append(case[i])
-    
-    exp.append(str(num[-1]))
-    str_exp = ''.join(exp)
-    result.append(eval(str_exp))
+        res = calc(res, num[i+1], case[i])
+    result.append(res)
 
 print(max(result))
 print(min(result))
