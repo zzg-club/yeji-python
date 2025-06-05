4e11aec357ef5a0125b050a52d72cd4d25122b61
solux <lee.yeji0327@gmail.com>
2025-06-05 17:34:45 +0900

    feat: sort key 활용

--- itscote/sort/sort.py
+++ itscote/sort/sort.py
@@ -73,3 +73,22 @@ for i in range(len(array)):
 for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
     for j in range(count[i]):
         print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+
+
+# sorted
+result = sorted(array)
+print(result)
+
+array.sort()
+print(array)
+
+
+# key를 활용한 코드
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
+
+def setting(data):
+    return data[1]
+
+result = sorted(array, key=setting)
+print(result)
+
