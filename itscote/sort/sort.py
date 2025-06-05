3fc59b047da15a0465a6d22cd6f48a527eabf24b
solux <lee.yeji0327@gmail.com>
2025-06-05 15:48:50 +0900

    feat: 삽입정렬 예제코드

--- itscote/sort/sort.py
+++ itscote/sort/sort.py
@@ -1,11 +1,21 @@
-# 선택 정렬
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
+# 선택 정렬
 for i in range(len(array)):
     min_index = i # 가장 작은 원소의 인덱스
     for j in range(i + 1, len(array)):
         if array[min_index] > array[j]:
             min_index = j
     array[i], array[min_index] = array[min_index], array[i] # 스와프
+
+print(array)
+
+# 삽입 정렬
+for i in range(1, len(array)):
+    for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하며 반복하는 문법
+        if array[j] < array[j-1]: # 왼쪽 데이터가 자기보다 크면 한 칸씩 왼쪽으로 이동
+            array[j], array[j-1] = array[j-1], array[j]
+        else: # 자기보다 작은 데이터를 만나면 그 위치에 멈춤
+            break
 
 print(array)
