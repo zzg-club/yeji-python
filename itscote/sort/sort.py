80b55c1ab8101aa3cffa21306eb8c57c871289b4
solux <lee.yeji0327@gmail.com>
2025-06-05 16:47:19 +0900

    feat: 계수정렬

--- itscote/sort/sort.py
+++ itscote/sort/sort.py
@@ -62,3 +62,14 @@ def quick_sort(array):
 
 print(quick_sort(array))
 
+
+# 계수 정렬
+# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1 # 각 데이터에 해당하는 인덱스의 값 증가
+
+for i in range(len(count)): # 리스트에 기록된 정렬 정보 확인
+    for j in range(count[i]):
+        print(i, end=' ') # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
