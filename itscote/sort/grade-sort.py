4f0bf7f820da628d8153b9a5935c622c5242f6fc
solux <lee.yeji0327@gmail.com>
2025-06-08 14:11:37 +0900

    feat: 실전-03 성적이 낮은 순서로 학생 출력하기 교재 답안 추가

--- itscote/sort/grade-sort.py
+++ itscote/sort/grade-sort.py
@@ -7,3 +7,21 @@ grade.sort(key=lambda x:int(x[1]))
 
 for name, score in grade:
     print(name, end=' ')
+
+# --------------------------------------
+# 교재 답안
+n = int(input())
+
+# N명의 학생 정보를 입력받아 리스트에 저장
+array = []
+for i in range(n):
+    input_data = input().split()
+    # 이름은 문자열 그대로, 점수는 정수형으로 변환하여 저장
+    array.append(input_data[0], int(input_data[1]))
+
+# 키(key)를 이용하여, 점수를 기준으로 정렬
+array = sorted(array, key=lambda student: student[1])
+
+# 정렬이 수행된 결과를 출력
+for student in array:
+    print(student[0], end=' ')
